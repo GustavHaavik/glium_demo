8af6fbe991670ffbd6ecd6f59e46f0050e252990
gustavhaavik <gkhaavik@gmail.com>
2024-01-31 22:32:46 +0100

    we've got Blinn-Phong shading, which include diffussion and specular

--- src/main.rs
+++ src/main.rs
@@ -38,13 +38,17 @@ fn main() {
         in vec3 normal;
 
         out vec3 v_normal;
+        out vec3 v_position;
 
-        uniform mat4 perspective;       // new
-        uniform mat4 matrix;
+        uniform mat4 perspective;
+        uniform mat4 view;
+        uniform mat4 model;
 
         void main() {
-            v_normal = transpose(inverse(mat3(matrix))) * normal;
-            gl_Position = perspective * matrix * vec4(position, 1.0);       // new
+            mat4 modelview = view * model;
+            v_normal = transpose(inverse(mat3(modelview))) * normal;
+            gl_Position = perspective * modelview * vec4(position, 1.0);
+            v_position = gl_Position.xyz / gl_Position.w;
         }
     "#;
 
@@ -52,14 +56,24 @@ fn main() {
         #version 150
 
         in vec3 v_normal;
+        in vec3 v_position;
+        
         out vec4 color;
+
         uniform vec3 u_light;
 
+        const vec3 ambient_color = vec3(0.2, 0.0, 0.0);
+        const vec3 diffuse_color = vec3(0.6, 0.0, 0.0);
+        const vec3 specular_color = vec3(1.0, 1.0, 1.0);
+
         void main() {
-            float brightness = dot(normalize(v_normal), normalize(u_light));
-            vec3 dark_color = vec3(0.6, 0.0, 0.0);
-            vec3 regular_color = vec3(1.0, 0.0, 0.0);
-            color = vec4(mix(dark_color, regular_color, brightness), 1.0);
+            float diffuse = max(dot(normalize(v_normal), normalize(u_light)), 0.0);
+
+            vec3 camera_dir = normalize(-v_position);
+            vec3 half_direction = normalize(normalize(u_light) + camera_dir);
+            float specular = pow(max(dot(half_direction, normalize(v_normal)), 0.0), 16.0);
+
+            color = vec4(ambient_color + diffuse * diffuse_color + specular * specular_color, 1.0);
         }
     "#;
 
@@ -108,30 +122,16 @@ fn main() {
                 let mut frame = display.draw();
                 frame.clear_color_and_depth((0.0, 0.0, 1.0, 1.0), 1.0);
 
-                let matrix = [
+                let model = [
                     [0.01, 0.0, 0.0, 0.0],
                     [0.0, 0.01, 0.0, 0.0],
                     [0.0, 0.0, 0.01, 0.0],
-                    [0.0, 0.0, 2.5, 1.0f32],
+                    [0.0, 0.0, 2.0, 1.0f32],
                 ];
 
-                let perspective = {
-                    let (width, height) = frame.get_dimensions();
-                    let aspect_ratio = height as f32 / width as f32;
-
-                    let fov: f32 = 3.141592 / 3.0;
-                    let zfar = 1024.0;
-                    let znear = 0.1;
-
-                    let f = 1.0 / (fov / 2.0).tan();
-
-                    [
-                        [f * aspect_ratio, 0.0, 0.0, 0.0],
-                        [0.0, f, 0.0, 0.0],
-                        [0.0, 0.0, (zfar + znear) / (zfar - znear), 1.0],
-                        [0.0, 0.0, -(2.0 * zfar * znear) / (zfar - znear), 0.0],
-                    ]
-                };
+                let (width, height) = frame.get_dimensions();
+                let aspect_ratio = height as f32 / width as f32;
+                let perspective = perspective_matrix(aspect_ratio);
 
                 // the direction of the light
                 let light = [-1.0, 0.4, 0.9f32];
@@ -142,15 +142,21 @@ fn main() {
                         write: true,
                         ..Default::default()
                     },
+                    // DISABLED FOR NOW because:
+                    // You can look through holes and not see anything inside.
+                    // 3D models are usually entirely closed, but not our teapot.
+                    // backface_culling: glium::draw_parameters::BackfaceCullingMode::CullClockwise,
                     ..Default::default()
                 };
+
+                let view = view_matrix(&[2.0, -1.0, 1.0], &[-2.0, 1.0, 1.0], &[0.0, 1.0, 0.0]);
 
                 frame
                     .draw(
                         (&positions, &normals),
                         &indices,
                         &program,
-                        &uniform! { matrix: matrix, perspective: perspective, u_light: light },
+                        &uniform! { model: model, view: view, perspective: perspective, u_light: light },
                         &params,
                     )
                     .unwrap();
@@ -163,6 +169,61 @@ fn main() {
             _ => (),
         }
     });
+}
+
+fn perspective_matrix(aspect_ratio: f32) -> [[f32; 4]; 4] {
+    let fov: f32 = 3.141592 / 3.0;
+    let zfar = 1024.0;
+    let znear = 0.1;
+
+    let f = 1.0 / (fov / 2.0).tan();
+
+    [
+        [f * aspect_ratio, 0.0, 0.0, 0.0],
+        [0.0, f, 0.0, 0.0],
+        [0.0, 0.0, (zfar + znear) / (zfar - znear), 1.0],
+        [0.0, 0.0, -(2.0 * zfar * znear) / (zfar - znear), 0.0],
+    ]
+}
+
+fn view_matrix(position: &[f32; 3], direction: &[f32; 3], up: &[f32; 3]) -> [[f32; 4]; 4] {
+    let f = {
+        let f = direction;
+        let len = f[0] * f[0] + f[1] * f[1] + f[2] * f[2];
+        let len = len.sqrt();
+        [f[0] / len, f[1] / len, f[2] / len]
+    };
+
+    let s = [
+        up[1] * f[2] - up[2] * f[1],
+        up[2] * f[0] - up[0] * f[2],
+        up[0] * f[1] - up[1] * f[0],
+    ];
+
+    let s_norm = {
+        let len = s[0] * s[0] + s[1] * s[1] + s[2] * s[2];
+        let len = len.sqrt();
+        [s[0] / len, s[1] / len, s[2] / len]
+    };
+
+    let u = [
+        f[1] * s_norm[2] - f[2] * s_norm[1],
+        f[2] * s_norm[0] - f[0] * s_norm[2],
+        f[0] * s_norm[1] - f[1] * s_norm[0],
+    ];
+
+    let p = [
+        -position[0] * s_norm[0] - position[1] * s_norm[1] - position[2] * s_norm[2],
+        -position[0] * u[0] - position[1] * u[1] - position[2] * u[2],
+        -position[0] * f[0] - position[1] * f[1] - position[2] * f[2],
+    ];
+
+    [
+        [s_norm[0], u[0], f[0], 0.0],
+        [s_norm[1], u[1], f[1], 0.0],
+        [s_norm[2], u[2], f[2], 0.0],
+        [p[0], p[1], p[2], 1.0],
+    ]
 }
 
 #[derive(Copy, Clone)]
