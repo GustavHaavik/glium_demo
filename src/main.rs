f0aeb68751d807b16be4d969369f49004bc75fd0
gustavhaavik <gkhaavik@gmail.com>
2024-01-31 22:53:04 +0100

    epic wall

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
+use std::io::Cursor;
+
 use glium::backend::glutin::SimpleWindowBuilder;
-use glium::index::NoIndices;
-use glium::index::PrimitiveType;
+
 use glium::Depth;
 use glium::DepthTest;
 use glium::DrawParameters;
 use glium::Program;
 use glium::Surface;
-use glium::VertexBuffer;
+
 use winit::event::ElementState;
 use winit::event::Event;
 use winit::event::KeyEvent;
@@ -22,29 +23,82 @@ fn main() {
     let event_loop = winit::event_loop::EventLoopBuilder::new().build().unwrap();
     let (window, display) = SimpleWindowBuilder::new().build(&event_loop);
 
-    let positions = glium::VertexBuffer::new(&display, &teapot::VERTICES).unwrap();
-    let normals = glium::VertexBuffer::new(&display, &teapot::NORMALS).unwrap();
-    let indices = glium::IndexBuffer::new(
+    // FOR DISPLAYING 3D MODELS
+    // let positions = glium::VertexBuffer::new(&display, &teapot::VERTICES).unwrap();
+    // let normals = glium::VertexBuffer::new(&display, &teapot::NORMALS).unwrap();
+    // let indices = glium::IndexBuffer::new(
+    //     &display,
+    //     glium::index::PrimitiveType::TrianglesList,
+    //     &teapot::INDICES,
+    // )
+    // .unwrap();
+
+    let shape = glium::vertex::VertexBuffer::new(
         &display,
-        glium::index::PrimitiveType::TrianglesList,
-        &teapot::INDICES,
+        &[
+            Vertex {
+                position: [-1.0, 1.0, 0.0],
+                normal: [0.0, 0.0, -1.0],
+                tex_coords: [0.0, 1.0],
+            },
+            Vertex {
+                position: [1.0, 1.0, 0.0],
+                normal: [0.0, 0.0, -1.0],
+                tex_coords: [1.0, 1.0],
+            },
+            Vertex {
+                position: [-1.0, -1.0, 0.0],
+                normal: [0.0, 0.0, -1.0],
+                tex_coords: [0.0, 0.0],
+            },
+            Vertex {
+                position: [1.0, -1.0, 0.0],
+                normal: [0.0, 0.0, -1.0],
+                tex_coords: [1.0, 0.0],
+            },
+        ],
     )
     .unwrap();
+
+    let image = image::load(
+        Cursor::new(&include_bytes!("../assets/diffuse.jpg")),
+        image::ImageFormat::Jpeg,
+    )
+    .unwrap()
+    .to_rgba8();
+    let image_dimensions = image.dimensions();
+    let image =
+        glium::texture::RawImage2d::from_raw_rgba_reversed(&image.into_raw(), image_dimensions);
+    let diffuse_texture = glium::texture::SrgbTexture2d::new(&display, image).unwrap();
+
+    let image = image::load(
+        std::io::Cursor::new(&include_bytes!("../assets/normal.png")),
+        image::ImageFormat::Png,
+    )
+    .unwrap()
+    .to_rgba8();
+    let image_dimensions = image.dimensions();
+    let image =
+        glium::texture::RawImage2d::from_raw_rgba_reversed(&image.into_raw(), image_dimensions);
+    let normal_map = glium::texture::Texture2d::new(&display, image).unwrap();
 
     let vertex_shader_src = r#"
         #version 150
 
         in vec3 position;
         in vec3 normal;
+        in vec2 tex_coords;
 
         out vec3 v_normal;
         out vec3 v_position;
+        out vec2 v_tex_coords;
 
         uniform mat4 perspective;
         uniform mat4 view;
         uniform mat4 model;
 
         void main() {
+            v_tex_coords = tex_coords;
             mat4 modelview = view * model;
             v_normal = transpose(inverse(mat3(modelview))) * normal;
             gl_Position = perspective * modelview * vec4(position, 1.0);
@@ -57,21 +111,48 @@ fn main() {
 
         in vec3 v_normal;
         in vec3 v_position;
-        
+        in vec2 v_tex_coords;
+
         out vec4 color;
 
         uniform vec3 u_light;
+        uniform sampler2D diffuse_tex;
+        uniform sampler2D normal_tex;
 
-        const vec3 ambient_color = vec3(0.2, 0.0, 0.0);
-        const vec3 diffuse_color = vec3(0.6, 0.0, 0.0);
+        // For now, we'll use a constant ambient and diffuse value
+        // const vec3 ambient_color = vec3(0.2, 0.0, 0.0);
+        // const vec3 diffuse_color = vec3(0.6, 0.0, 0.0);
         const vec3 specular_color = vec3(1.0, 1.0, 1.0);
 
+        mat3 cotangent_frame(vec3 normal, vec3 pos, vec2 uv) {
+            vec3 dp1 = dFdx(pos);
+            vec3 dp2 = dFdy(pos);
+            vec2 duv1 = dFdx(uv);
+            vec2 duv2 = dFdy(uv);
+
+            vec3 dp2perp = cross(dp2, normal);
+            vec3 dp1perp = cross(normal, dp1);
+            vec3 T = dp2perp * duv1.x + dp1perp * duv2.x;
+            vec3 B = dp2perp * duv1.y + dp1perp * duv2.y;
+
+            float invmax = inversesqrt(max(dot(T, T), dot(B, B)));
+            return mat3(T * invmax, B * invmax, normal);
+        }
+
         void main() {
-            float diffuse = max(dot(normalize(v_normal), normalize(u_light)), 0.0);
+            vec3 diffuse_color = texture(diffuse_tex, v_tex_coords).rgb;
+            vec3 ambient_color = diffuse_color * 0.1;
+
+            vec3 v_normal_unit = normalize(v_normal);
+            vec3 normal_map = texture(normal_tex, v_tex_coords).rgb;
+            mat3 tbn = cotangent_frame(v_normal_unit, -v_position, v_tex_coords);
+            vec3 real_normal = normalize(tbn * -(normal_map * 2.0 - 1.0));
+
+            float diffuse = max(dot(real_normal, normalize(u_light)), 0.0);
 
             vec3 camera_dir = normalize(-v_position);
             vec3 half_direction = normalize(normalize(u_light) + camera_dir);
-            float specular = pow(max(dot(half_direction, normalize(v_normal)), 0.0), 16.0);
+            float specular = pow(max(dot(half_direction, real_normal), 0.0), 16.0);
 
             color = vec4(ambient_color + diffuse * diffuse_color + specular * specular_color, 1.0);
         }
@@ -80,17 +161,17 @@ fn main() {
     let program =
         Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
 
-    let image = image::load(
-        std::io::Cursor::new(&include_bytes!("../assets/meandcat.jpg")[..]),
-        image::ImageFormat::Jpeg,
-    )
-    .unwrap()
-    .to_rgba8();
-    let image_dimensions = image.dimensions();
-    let image =
-        glium::texture::RawImage2d::from_raw_rgba_reversed(&image.into_raw(), image_dimensions);
+    // let image = image::load(
+    //     std::io::Cursor::new(&include_bytes!("../assets/meandcat.jpg")[..]),
+    //     image::ImageFormat::Jpeg,
+    // )
+    // .unwrap()
+    // .to_rgba8();
+    // let image_dimensions = image.dimensions();
+    // let image =
+    //     glium::texture::RawImage2d::from_raw_rgba_reversed(&image.into_raw(), image_dimensions);
 
-    let texture = glium::texture::SrgbTexture2d::new(&display, image).unwrap();
+    let start = std::time::Instant::now();
 
     let _ = event_loop.run(move |event, elwt| {
         match event {
@@ -122,11 +203,14 @@ fn main() {
                 let mut frame = display.draw();
                 frame.clear_color_and_depth((0.0, 0.0, 1.0, 1.0), 1.0);
 
+                let t = (std::time::Instant::now() - start).as_secs_f32() * 2.0;
+                let ang = t.sin();
+                let (c, s) = (ang.cos(), ang.sin());
                 let model = [
-                    [0.01, 0.0, 0.0, 0.0],
-                    [0.0, 0.01, 0.0, 0.0],
-                    [0.0, 0.0, 0.01, 0.0],
-                    [0.0, 0.0, 2.0, 1.0f32],
+                    [c, 0.0, s, 0.0],
+                    [0.0, 1.0, 0.0, 0.0],
+                    [-s, 0.0, c, 0.0],
+                    [0.0, 0.0, 0.0, 1.0f32],
                 ];
 
                 let (width, height) = frame.get_dimensions();
@@ -149,14 +233,15 @@ fn main() {
                     ..Default::default()
                 };
 
-                let view = view_matrix(&[2.0, -1.0, 1.0], &[-2.0, 1.0, 1.0], &[0.0, 1.0, 0.0]);
-
+                let view = view_matrix(&[0.5, 0.2, -3.0], &[-0.5, -0.2, 3.0], &[0.0, 1.0, 0.0]);
+                
                 frame
                     .draw(
-                        (&positions, &normals),
-                        &indices,
+                        &shape,
+                        glium::index::NoIndices(glium::index::PrimitiveType::TriangleStrip),
                         &program,
-                        &uniform! { model: model, view: view, perspective: perspective, u_light: light },
+                        &uniform! { model: model, view: view, perspective: perspective,
+                        u_light: light, diffuse_tex: &diffuse_texture },
                         &params,
                     )
                     .unwrap();
@@ -226,10 +311,20 @@ fn view_matrix(position: &[f32; 3], direction: &[f32; 3], up: &[f32; 3]) -> [[f3
     ]
 }
 
+// FOR DISPLAYING 3D MODELS
+// #[derive(Copy, Clone)]
+// struct Vertex {
+//     position: [f32; 2],
+//     tex_coords: [f32; 2],
+// }
+
+// implement_vertex!(Vertex, position, tex_coords);
+
 #[derive(Copy, Clone)]
 struct Vertex {
-    position: [f32; 2],
+    position: [f32; 3],
+    normal: [f32; 3],
     tex_coords: [f32; 2],
 }
 
-implement_vertex!(Vertex, position, tex_coords);
+implement_vertex!(Vertex, position, normal, tex_coords);
