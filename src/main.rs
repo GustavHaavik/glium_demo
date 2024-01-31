e908cccd93346e699f1334e46e1688e6b670cdd1
gustavhaavik <gkhaavik@gmail.com>
2024-01-31 21:33:32 +0100

    now with colors lets gooo

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,12 @@ use glium::index::PrimitiveType;
 use glium::Program;
 use glium::Surface;
 use glium::VertexBuffer;
+use winit::event::ElementState;
 use winit::event::Event;
+use winit::event::KeyEvent;
 use winit::event::WindowEvent;
+use winit::keyboard::KeyCode;
+use winit::keyboard::PhysicalKey;
 
 #[macro_use]
 extern crate glium;
@@ -13,16 +17,20 @@ fn main() {
     let event_loop = winit::event_loop::EventLoopBuilder::new().build().unwrap();
     let (window, display) = SimpleWindowBuilder::new().build(&event_loop);
 
-    let vertex1 = Vertex {
-        position: [-0.5, -0.5],
-    };
-    let vertex2 = Vertex {
-        position: [0.0, 0.5],
-    };
-    let vertex3 = Vertex {
-        position: [0.5, -0.5],
-    };
-    let shape = vec![vertex1, vertex2, vertex3];
+    let shape = vec![
+        Vertex {
+            position: [-0.5, -0.5],
+            color: [1.0, 0.0, 0.0],
+        },
+        Vertex {
+            position: [0.0, 0.5],
+            color: [0.0, 1.0, 0.0],
+        },
+        Vertex {
+            position: [0.5, -0.5],
+            color: [0.0, 0.0, 1.0],
+        },
+    ];
 
     let vertex_buffer = VertexBuffer::new(&display, &shape).unwrap();
     let indices = NoIndices(PrimitiveType::TrianglesList);
@@ -31,48 +39,48 @@ fn main() {
         #version 150
 
         in vec2 position;
+        in vec3 color;      // our new attribute
+        out vec3 vertex_color;
 
-        uniform float x;
+        uniform mat4 matrix;
 
         void main() {
-            vec2 pos = position;
-            pos.x += x;
-            gl_Position = vec4(pos, 0.0, 1.0);
+            vertex_color = color; // we need to set the value of each `out` variable.
+            gl_Position = matrix * vec4(position, 0.0, 1.0);
         }
     "#;
 
     let fragment_shader_src = r#"
         #version 150
 
+        in vec3 vertex_color;
         out vec4 color;
 
         void main() {
-            color = vec4(1.0, 0.0, 0.0, 1.0);
+            color = vec4(vertex_color, 1.0);   // We need an alpha value as well
         }
     "#;
 
     let program =
         Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
 
-    let mut target = display.draw();
-    target.clear_color(0.0, 0.0, 1.0, 1.0);
-    target
-        .draw(
-            &vertex_buffer,
-            &indices,
-            &program,
-            &glium::uniforms::EmptyUniforms,
-            &Default::default(),
-        )
-        .unwrap();
-    target.finish().unwrap();
-
     let mut t: f32 = 0.0;
 
     let _ = event_loop.run(move |event, elwt| {
         match event {
             Event::WindowEvent {
-                event: WindowEvent::CloseRequested,
+                event:
+                    WindowEvent::CloseRequested
+                    | WindowEvent::KeyboardInput {
+                        event:
+                            KeyEvent {
+                                physical_key: PhysicalKey::Code(KeyCode::Escape),
+                                state: ElementState::Pressed,
+                                repeat: false,
+                                ..
+                            },
+                        ..
+                    },
                 ..
             } => {
                 println!("The close button was pressed; stopping");
@@ -95,20 +103,31 @@ fn main() {
                 // Draw triangle
                 t += 0.08;
 
-                let x_offset = t.sin() * 0.5;
+                let x = t.sin() * 0.5;
 
-                let mut target = display.draw();
-                target.clear_color(0.0, 0.0, 1.0, 1.0);
-                target
+                let mut frame = display.draw();
+                frame.clear_color(0.0, 0.0, 1.0, 1.0);
+
+                let uniforms = uniform! {
+                    matrix: [
+                        [ 1.0, 0.0, 0.0, 0.0 ],
+                        [0.0, 1.0, 0.0, 0.0],
+                        [0.0, 0.0, 1.0, 0.0],
+                        [0.0, 0.0, 0.0, 1.0f32],
+                    ]
+                };
+
+                frame
                     .draw(
                         &vertex_buffer,
                         &indices,
                         &program,
-                        &uniform! { x: x_offset },
+                        &uniforms,
                         &Default::default(),
                     )
                     .unwrap();
-                target.finish().unwrap();
+
+                frame.finish().unwrap();
             }
             // show_image::event::Event::RedrawEventsCleared => {
             //     window.request_redraw();
@@ -121,5 +140,6 @@ fn main() {
 #[derive(Copy, Clone)]
 struct Vertex {
     position: [f32; 2],
+    color: [f32; 3],
 }
-implement_vertex!(Vertex, position);
+implement_vertex!(Vertex, position, color);
